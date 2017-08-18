270d871bb4c8a3494078799aeea32617bd742578
prasadmodi-pm <prasadmodi.pm@gmail.com>
2017-08-18 17:24:06 -0400

    Delete CallDB.py

--- CallDB.py
+++
@@ -1,5 +0,0 @@
-from Sql import createtable, insertinto,connection
-from Func import tab
-
-tab(table_name='RJP', table_opr= connection.execute("INSERT INTO RJP (SID,SNAME,SAGE,SADDRESS) \
-        VALUES (12, 'Bola', 42, 'NJ')"))
